7ff62beac058bc906fef8eaab20e7132db136360
IsabellaCazzamali <isa.cazzamali@gmail.com>
2024-01-19 12:28:46 +0100

    initial commit add h1

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1 class="title">Hello, world</h1>
+    </div>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
+
+</template>
+
+<style>
+
+</style>
